5f8c3e41d4b142ad4466be0b5f5a182ae4831dc7
miguelzapp <miguel.cortina.zapp@gmail.com>
2023-08-22 10:49:29 +0200

    Add auth variable modules

---
+++ auth.py
@@ -0,0 +1,5 @@
+version = "1.0.0"
+agent = f"vertretungsplan-rsbam/{version}"
+
+lehrerToken = "Bearer <your_generated_token>"
+verwaltungToken = "Bearer <your_generated_token>"
---
+++ db_auth.py
@@ -0,0 +1,5 @@
+database = "<your_database_name>"
+host = "127.0.0.1"
+
+username = "<your_database_username>"
+password = "<your_database_password>"
